3238e15742c806da6a5cebcc6fbafaa04b876271
Butko Denis <butko.dv@gmail.com>
2024-05-14 23:03:29 +0300

    chore: Update MongoDB connection URL and fix nodemon script in package.json

--- server/index.js
+++ server/index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+
 const app = express();
 require('dotenv').config();
 
 const { MONGO_URL, PORT } = process.env;
 
+mongoose
+  .connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB підключено успішно'))
+  .catch((err) => console.error(err));
+
+app.use(cors());
+app.use(express.json());
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
